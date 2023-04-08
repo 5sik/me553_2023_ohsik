5ae31c823629c49a96a2c6810f47896fd8ef5fda
5sik <yt2281@naver.com>
2023-04-08 16:50:33 +0900

    first of all, I finished omega

--- solutions/exercise2_20233460.hpp
+++ solutions/exercise2_20233460.hpp
@@ -40,18 +40,37 @@ inline Eigen::Vector3d getFootAngularVelocity (const Eigen::VectorXd& gc, const 
   //////////////////////////
   ///// Your Code Here /////
   //////////////////////////
-  Eigen::Matrix<3,3> R ;
+
+  /// 풀이 순서 -> 1. 다리가 안 돌아가 있다는 전제 하(Axis가 돌아가 있지 않음)에 각속도 구하기 (축 맞춰서)
+  ///         -> 2. 기본 gc에 의해 축이 돌아가 있는거 고려해서 적용
+
+  Eigen::Vector3d x; // "axis"
+  Eigen::Vector3d w; // theta_dot * axis = omega ("Angular Velocity") 로 사용
+  raisim::Mat<3,3> R ; // gc값에 의해 돌아가는 축 고려해주는 Matrix로 사용
+
+  x << 0,1,0; // for knee joint axis
+  w << gv[8] * x; //Angular velocity of knee joint without theta change
+  raisim::angleAxisToRotMat(x,gc[9],R);
+  w<< R.e() * w; // omega for knee joint of Aliengo
+
+  x << 0,1,0; // for pitch joint axis
+  w << w + gv[7] * x ; //Angular velocity of pitch joint without theta change
+  raisim::angleAxisToRotMat(x,gc[8],R);
+  w << R.e() * w;
+
+  x << 1,0,0; // for hip joint axis
+  w << w + gv[6] * x;
+  raisim::angleAxisToRotMat(x,gc[7],R);
+  w << R.e() * w;
 
   raisim::Vec<4> v;// for 쿼터니안
-  raisim::Mat<3,3> R00;
-
   for(int i=0;i<4;i++);
     v[i] = gc[i+3];
-  raisim::quatToRotMat(v,R00)  // 00' 의 Rotational Matrix
+  raisim::quatToRotMat(v,R)  // 00'(Trunk) 의 Rotational Matrix
 
+  w << R.e() * w ; // Trunk axis 회전까지 고려
+  w << w + Eigen::Vector3d{gv[3].gv[4],gv[5]}; // Trunk Angular Velocity 고려
 
-  R = R00.e() + sex + sex + sex
-
-  return Eigen::Vector3d::Ones(); /// replace this
+  return w; /// replace this
 }
 
